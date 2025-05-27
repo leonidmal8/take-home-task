71c168784a4d6d8743c8b9f7addf89c592cfb8bf
leonidmal8 <50879609+leonidmal8@users.noreply.github.com>
2025-05-27 21:00:23 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -58,3 +58,29 @@ output "docker_push_commands" {
     "docker push ${aws_ecr_repository.main.repository_url}:latest"
   ]
 }
+
+output "cognito_user_pool_id" {
+  description = "ID of the Cognito User Pool"
+  value       = aws_cognito_user_pool.main.id
+}
+
+output "cognito_user_pool_client_id" {
+  description = "ID of the Cognito User Pool Client"
+  value       = aws_cognito_user_pool_client.main.id
+  sensitive   = true
+}
+
+output "cognito_user_pool_domain" {
+  description = "Domain of the Cognito User Pool"
+  value       = aws_cognito_user_pool_domain.main.domain
+}
+
+output "cognito_login_url" {
+  description = "Cognito hosted UI login URL"
+  value       = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.main.id}&response_type=code&scope=openid+email+profile&redirect_uri=${var.callback_urls[0]}"
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.main.dns_name
+}
