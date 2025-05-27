b6d68bf2dc77b26b81f273881dfb8f14346c3062
leonidmal8 <50879609+leonidmal8@users.noreply.github.com>
2025-05-27 21:12:30 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "environment" {
@@ -14,7 +14,7 @@ variable "environment" {
 variable "app_name" {
   description = "Name of the application"
   type        = string
-  default     = "home-task-webapp"
+  default     = "home-task"
 }
 
 variable "image_tag" {
@@ -26,5 +26,5 @@ variable "image_tag" {
 variable "certificate_arn" {
   description = "ARN of the SSL certificate for HTTPS (update with your actual certificate)"
   type        = string
-  default     = "arn:aws:acm:us-west-2:123456789012:certificate/12345678-1234-1234-1234-123456789012"
+  default     = ""
 }
